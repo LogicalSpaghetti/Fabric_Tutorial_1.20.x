d9b566f072c4d1f1ec6ea721baffd0d4179846bb
Logical Spaghetti <130314555+LogicalSpaghetti@users.noreply.github.com>
2024-02-19 19:11:17 -0500

    Initial commit

---
+++ src/main/java/us/fiel/TutorialModClient.java
@@ -0,0 +1,10 @@
+package us.fiel;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
